fa1dcbd8d2d73dbd393c53777d8d5d8a011bb58f
KustyBoo <93069812+KustyBoo@users.noreply.github.com>
2022-10-09 00:15:32 +0800

    Update konfirmasi-admin.php

--- konfirmasi-admin.php
+++ konfirmasi-admin.php
@@ -1,11 +1,9 @@
 <?php
-// (A) START SESSION
+
 session_start();
 
-// (B) LOGOUT REQUEST
 if (isset($_POST["logout"])) { unset($_SESSION["admin"]); }
 
-// (C) REDIRECT TO LOGIN PAGE IF NOT LOGGED IN
 if (!isset($_SESSION["admin"])) {
   header("Location: login-admin.php");
   exit();
