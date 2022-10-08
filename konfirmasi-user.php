8f41b52fa94acd351380bd2918f16d6fabc7d938
KustyBoo <93069812+KustyBoo@users.noreply.github.com>
2022-10-09 00:15:44 +0800

    Update konfirmasi-user.php

--- konfirmasi-user.php
+++ konfirmasi-user.php
@@ -1,11 +1,9 @@
 <?php
-// (A) START SESSION
+
 session_start();
 
-// (B) LOGOUT REQUEST
 if (isset($_POST["logout"])) { unset($_SESSION["user"]); }
 
-// (C) REDIRECT TO LOGIN PAGE IF NOT LOGGED IN
 if (!isset($_SESSION["user"])) {
   header("Location: login-user.php");
   exit();
