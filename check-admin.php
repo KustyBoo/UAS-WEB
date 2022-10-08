06475d4a359e275e6dce20803e7adea77aaabc4d
KustyBoo <93069812+KustyBoo@users.noreply.github.com>
2022-10-09 00:13:34 +0800

    Update check-admin.php

--- check-admin.php
+++ check-admin.php
@@ -1,11 +1,9 @@
 <?php
-// (A) START SESSION
+
 session_start();
 
-// (B) HANDLE LOGIN
 
 if (isset($_POST["admin"]) && !isset($_SESSION["admin"])) {
-  // (B1) USERS & PASSWORDS - SET YOUR OWN !
   $admin = [
     "admin1" => "123456",
     "admin2" => "654321",
@@ -21,9 +19,7 @@ if (isset($_POST["admin"]) && !isset($_SESSION["admin"])) {
   if (!isset($_SESSION["admin"])) { $failed = true; }
 }
 
-// (C) REDIRECT USER TO HOME PAGE IF SIGNED IN
-
 if (isset($_SESSION["admin"])) {
-  header("Location: index1.php"); // SET YOUR OWN HOME PAGE!
+  header("Location: index1.php"); 
   exit();
 }
